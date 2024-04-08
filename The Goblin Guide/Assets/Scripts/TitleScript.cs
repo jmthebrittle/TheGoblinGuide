3b2ddd2a5496d6a6578ee7ed9edebe57852bcfcc
JM <134972736+200871010@users.noreply.github.com>
2024-04-08 12:13:55 +1200

    Added a Title that freezes

---
+++ The Goblin Guide/Assets/Scripts/TitleScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScript : MonoBehaviour
+{
+
+    public Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // to disable
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            rigidbody.isKinematic = false;
+        }
+    }
+}
